626a1c4c847d49db0a3a9b096c8deb3386e54bec
Johannes <Johannes@PC-JB.lan>
2011-11-10 18:37:13 +0100

    Revert "Test Commit auf den Master"
    This reverts commit 827bad41fa2ef9108f641a1eadefc3832b5f8036.

--- src/main/java/jku/se/drilldown/ui/StructureDrilldownComponent.java
+++ src/main/java/jku/se/drilldown/ui/StructureDrilldownComponent.java
@@ -1,10 +1,6 @@
+//Untere Komponente, zeigt Projekte/Pakete/Dateien an, abgeleitet von DrilldownComponent
 package jku.se.drilldown.ui;
 
-/**
- * 
- * @author Johannes
- *
- */
 public class StructureDrilldownComponent extends DrilldownComponent{
 	
 	public StructureDrilldownComponent(){
