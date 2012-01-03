e69274f51e5e572476067205cad0847befb8d7e8
Markus <markus.adelsberger@gmail.com>
2012-01-03 09:15:37 +0100

    Quality und Benchmarkplugin auf ein gemeinsames Zusammengefuehrt

--- src/main/java/jku/se/drilldown/BenchmarkPage.java
+++ src/main/java/jku/se/drilldown/BenchmarkPage.java
@@ -4,13 +4,13 @@ import org.sonar.api.web.GwtPage;
 import org.sonar.api.web.NavigationSection;
 
 @NavigationSection(NavigationSection.RESOURCE)
-public class XMLTest extends GwtPage{
+public class BenchmarkPage extends GwtPage{
 
 	public String getTitle() {
-		return "XML Test";
+		return "Benchmark";
 	}
 
 	public String getGwtId() {
-		return "jku.se.drilldown.XMLTest";
+		return "jku.se.drilldown.BenchmarkPage";
 	}
 }
---
+++ src/main/java/jku/se/drilldown/DrilldownPlugin.java
@@ -0,0 +1,29 @@
+package jku.se.drilldown;
+
+import java.util.Arrays;
+import java.util.List;
+
+import jku.se.drilldown.batch.BenchmarkSensor;
+import jku.se.drilldown.batch.DrilldownMetrics;
+import jku.se.drilldown.batch.QMDrilldownDecorator;
+import jku.se.drilldown.batch.QMDrilldownSensor;
+
+public class DrilldownPlugin {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public List getExtensions() {
+		return Arrays.asList(	
+			// Definitions
+		    DrilldownMetrics.class,
+
+		    // Batch
+		    QMDrilldownSensor.class, 
+		    QMDrilldownDecorator.class,
+		    BenchmarkSensor.class,
+
+		    // UI
+		    QMDrilldownPage.class,
+		    BenchmarkPage.class
+		    );
+	}
+}
